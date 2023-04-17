8096ab39deca82f4b6e41d60ae0eb0b9581c3a5f
Jaime Díez <jaimecuentacod@gmail.com>
2023-04-17 13:00:48 +0200

    Count sheep exercise

---
+++ typescript/if-you-cant-sleep.ts
@@ -0,0 +1,17 @@
+/*If you can't sleep, just count sheep!!
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". 
+Input will always be valid, i.e. no negative integers.*/
+
+
+function countSheep(num: number): string {
+
+    let result: string = '';
+    
+    for (let i = 1; i <= num; i++) {
+        result += i.toString() + ' sheep...';
+    }
+    return result;
+    }
+
+console.log(countSheep(2))
