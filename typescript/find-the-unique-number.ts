691aea9646610bd11b59c80c05bbc0e116e67b10
Jaime Díez <jaimecuentacod@gmail.com>
2023-04-24 11:50:28 +0200

    Find the unique number exercise

---
+++ typescript/find-the-unique-number.ts
@@ -0,0 +1,17 @@
+export function findUniq(arr: number[]): number {
+    
+    const numberSet: Set<number> = new Set();
+
+    for (let num of arr) {
+        if (numberSet.size === 0 || numberSet.has(num)) {
+        numberSet.add(num);
+        } else {
+        return num;
+        }
+    }
+
+    return -1; // should never reach this point
+}
+
+console.log(findUniq([1, 1, 1, 2, 1, 1])); // returns 2
+console.log(findUniq([0, 0, 0.55, 0, 0])); // returns 0.55
